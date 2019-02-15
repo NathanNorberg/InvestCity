3889c48b34a689b6c3679a2165a7d537ac214717
Nathan <nugnorberg@gmail.com>
2019-02-14 18:52:27 -0700

    Initial Setup

--- README.md
+++ README.md
@@ -1,4 +1,73 @@
-# investcity
+# InvestCity
+
+## Project Description
+This capstone project InvestCity was completed in a 13 day period during the 6-month Galvanize Web Development program.  InvestCity allows for a company to create investment projects, specify who invested in it, the capital invested and update the investment progress so that the investors as well as the company can have a central location to easily view their investment information and print off reports on investments.
+
+## What problem does your project solve?
+This InvestCity, was created in collaboration with Cowley Norberg Investments (CNI) to track their investment properties and allows investors to see the ROI of each investment as well as receive updates on current investments. CNI lacked a centralized location for investment data as well as ease of use on the current website for both the admin side as well as the investor side. InvestCity provides a solution for CNI and other companies so that their investors can stay updated on investment progress and information.
+
+## How will your project solve this problem?
+InvestCity gives the CNI admin and investors a one stop site to be able to create new investment projects, edit them, add users (investros), view all past and current investments as well as view reports on all investments they have made..  Investors will be able to view individual investments details, the capital invested.
+
+## What inputs does it need?
+Investors:
+* Name
+* Email
+* Phone Number
+* Address (City, State)
+  * Investor Entity Name
+  * Address (Country, State, City)
+  * Contact Number
+  * Entity Notes
+Admin:
+* Creating / Editing Investments
+  * Type of Investment
+  * Grouping
+  * Investment Name
+  * Brief Description
+  * Purchase Date
+  * Purchase Price
+  * Investors Involved
+    * Amount Invested
+  * State
+  * County
+  * City
+  * Location
+  * Size
+  * Status (Current || Sold)
+  * Date Sold
+  * Detailed Description  
+* Investment Notes
+* Investment Documents
+* News / Announcements
+
+## What outputs does it produce?
+* Overall investment progress
+* Individual investment reports for each investor/entity
+
+
+## What technologies do you plan to use?
+* Firebase
+* Vue
+* Vuetify
+* Chartjs
+
+## Prioritized Feature list (Label stretch features with STRETCH)
+* Create Investor
+  * Add Investor Information
+* Add Entities to Investors
+* Create Investment Project
+  * Adding Investors
+  * Adding Investement Notes/Updates
+* Editing and Updating Investors/Investments
+* Adding News
+* Investor View to see all Involved Investments
+* Investor Edit Profile
+* Investor Add Notes to Project
+
+STRETCH
+* Full Report Outlining Deeper Project Details
+
 
 ## Project setup
 ```
--- src/router.js
+++ src/router.js
@@ -2,24 +2,62 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 
+import InvestorLogin from './component/InvestorLogin.vue'
+/*Investor Routes */
+import InvestorDashboard from './component/InvestorDashboard.vue'
+import InvestorProfile from './component/InvestorProfile.vue'
+import EditInvestorProfile from './component/EditInvestorProfile.vue'
+import InvestmentNotes from './component/InvestmentNotes.vue'
+import InvestorNews from './component/InvestorNews.vue'
+import InvestorReport from './component/InvestorReport.vue'
+/* Admin Routes */
+import AdminLogin from './component/AdminLogin.vue'
+import AdminDashboard from './component/AdminDashboard.vue'
+import AddNews from './component/AddNews.vue'
+import ViewNews from './component/ViewNews.vue'
+  /* Add, View, Edit Investments */
+import AddInvestment from './component/AddInvestment.vue'
+import AdminInvestmentDashboard from './component/AdminInvestmentDashboard.vue'
+import AdminNotes from './component/AdminNotes.vue'
+import InvestmentDetails from './component/InvestmentDetails.vue'
+import EditInvestmentDetails from './component/EditInvestmentDetails.vue'
+  /* Add, View, Edit Investors */
+import AddInvestor from './component/AddInvestor.vue'
+import AdminInvestorDashboard from './component/AdminInvestorDashboard.vue'
+import InvestorDetails from './component/InvestorDetails.vue'
+import EditInvestorDetails from './component/EditInvestorDetails.vue'
+
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+
+    { path: '/', component: InvestorLogin },
+    /*Investor Routes */
+    { path: '/investorDashboard', component: InvestorDashboard },
+    { path: '/investorProfile/:id', component: InvestorProfile },
+    { path: '/editInvestorProfile/:id', component: EditInvestorProfile },
+    { path: '/investorNotes/investment/:id', component: InvestmentNotes },
+    { path: '/investorNews', component: InvestorNews },
+    { path: '/investorReport', component: InvestorReport },
+    /* Admin Routes */
+    { path: '/adminLogin', component: AdminLogin },
+    { path: '/adminDashboard', component: AdminDashboard },
+    { path: '/addNews', component: AddNews },
+    { path: '/viewNews', component: ViewNews },
+      /* Add, View, Edit Investments */
+    { path: '/addInvestment', component: AddInvestment },
+    { path: '/adminInvestmentDashboard', component: AdminInvestmentDashboard },
+    { path: '/adminNotes/investment/:id', component: AdminNotes },
+    { path: '/investmentDetails/:id', component: InvestmentDetails },
+    { path: '/editInvestmentDetails/:id', component: EditInvestmentDetails },
+      /* Add, View, Edit Investors */
+    { path: '/addInvestor', component: AddInvestor },
+    { path: '/adminInvestorDashboard', component: AdminInvestmentDashboard },
+    { path: '/investorDetails/:id', component: InvestorDetails },
+    { path: '/editInvestorDetails/:id', component: EditInvestorDetails },
+
   ]
 })
